e6a31bd90d4761f48dbda5a69d9cd2a957d13eb3
sddilora <sumeyedilaradogan@gmail.com>
2023-11-27 18:36:11 +0300

    main fixed

--- main.go
+++ main.go
@@ -46,39 +46,3 @@ type Log struct {
     IsFile    bool
     CheckFunc func(string)
 }
-package checkher
-
-import (
-    "fmt"
-    "strings"
-)
-
-type FileChecker struct {
-}
-
-func NewFileChecker() FileChecker {
-    return &FileChecker{}
-}
-
-func (cFileChecker) Check(path string) {
-    if strings.Contains(path, " ") {
-        fmt.Println(false)
-    } else {
-        fmt.Println(true)
-    }
-}
-
-type LogChecker struct {
-}
-
-func NewLogChecker() LogChecker {
-    return &LogChecker{}
-}
-
-func (cLogChecker) Check(log string) {
-    if strings.Contains(log, "MESSAGE") {
-        fmt.Println(true)
-    } else {
-        fmt.Println(false)
-    }
-}
